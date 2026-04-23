fb5f5c4a654ff86b02cf87fdc14b39114eaa1224
WinstoniumAcc <winstonarcher2010@gmail.com>
2026-04-23 22:25:59 +0700

    serve static files

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/videos", express.static("videos"));
+app.use(express.static(path.join(__dirname)));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
